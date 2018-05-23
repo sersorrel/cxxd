eba239411c143901b67daa3a4cbc5d2a309c18e7
Josh Holland <anowlcalledjosh@gmail.com>
2018-05-23 16:07:17 +0100

    Add packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="cxxd",
+    version="1.0.0",
+    description="A colorized drop-in replacement for xxd.",
+    author="Sai Kalidindi",
+    url="https://github.com/skalidindi3/cxxd",
+    license="MIT",
+    classifiers=[
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    py_modules=["cxxd"],
+    entry_points={"console_scripts": ["cxxd=cxxd:main"]},
+)
